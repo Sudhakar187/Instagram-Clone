2622f45dff28239102bde2c9ba948ad7a934820b
Sudhakar187 <sv573713@gmail.com>
2026-03-16 09:11:57 +0530

    change some code

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const server = jsonServer.create();
 const router = jsonServer.router("data/jsondb.json");
 
-const middlewares = jsonServer.defaults({
-  static: "data"
-});
-
+const middlewares = jsonServer.defaults();
 server.use(middlewares);
+
+// serve assets folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.use("/assets", jsonServer.defaults().static(path.join(__dirname, "data/assets")));
+
 server.use(router);
 
 const port = process.env.PORT || 10000;
