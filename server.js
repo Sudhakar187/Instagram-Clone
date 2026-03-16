03336f175727bd7776a373e9723de9d36311ec97
Sudhakar187 <sv573713@gmail.com>
2026-03-16 09:08:08 +0530

    change server.js file

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ import jsonServer from "json-server";
 
 const server = jsonServer.create();
 const router = jsonServer.router("data/jsondb.json");
-const middlewares = jsonServer.defaults();
+
+const middlewares = jsonServer.defaults({
+  static: "data"
+});
 
 server.use(middlewares);
 server.use(router);
@@ -10,5 +13,5 @@ server.use(router);
 const port = process.env.PORT || 10000;
 
 server.listen(port, () => {
-  console.log(`JSON Server running on ${port}`);
+  console.log("JSON Server running on", port);
 });
--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,3 @@
 const API_URL = "https://instagram-clone-1-c78g.onrender.com";
+// const API_URL = "https://localhost:3000";
 export default API_URL;
